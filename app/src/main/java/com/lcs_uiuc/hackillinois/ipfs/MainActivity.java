511b4f98facc200ec6bb639f6d152d33e419d867
amagana8 <alexander.jm28@gmail.com>
2018-02-24 18:53:41 -0600

    changed side bar entries

--- app/src/main/java/com/lcs_uiuc/hackillinois/ipfs/MainActivity.java
+++ app/src/main/java/com/lcs_uiuc/hackillinois/ipfs/MainActivity.java
@@ -72,13 +72,14 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_home) {
+        } else if (id == R.id.nav_connections) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_contributions) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_report) {
+
+        } else if (id ==R.id.nav_settings) {
 
         } else if (id == R.id.nav_share) {
 
