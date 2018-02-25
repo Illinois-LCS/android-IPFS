d17cf1f139287c45589deb1bc75681f6fce33be3
javierg98 <#St36umpp1>
2018-02-25 10:22:02 -0600

    Configured fragments for navigation view

--- app/src/main/java/com/lcs_uiuc/hackillinois/ipfs/buttonReport.java
+++ app/src/main/java/com/lcs_uiuc/hackillinois/ipfs/buttonReport.java
@@ -1,53 +1,73 @@
 package com.lcs_uiuc.hackillinois.ipfs;
 
 import android.app.Fragment;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
+/**
+ * LCS-UIUC Hackillinois Project 2018
+ *
+ * resources used:
+ * https://github.com/codepath/android_guides/wiki/Fragment-Navigation-Drawer
+ *
+ *
+ */
 
 public class buttonReport extends android.support.v4.app.Fragment {
 
+    private OnButtonFragmentInteractionListener mListener;
+
+public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    return inflater.inflate(R.layout.fragment_report11, container, false);
+}
+
+public void onButtonPressed(View view) {
+
+    EditText mMain = (EditText) view.findViewById(R.id.editMain);
+    EditText mSubj = (EditText) view.findViewById(R.id.editSubj);
 
 
-    public void onClick (View view){
+    String sSubj = mSubj.getText().toString();
+    String sMain = mMain.getText().toString();
 
 
+    Intent i = new Intent(Intent.ACTION_SEND);
+    i.setType("message/rfc822");
+    i.putExtra(Intent.EXTRA_EMAIL, new String[]{"lcsatuiuc@gmail.com"});
+    i.putExtra(Intent.EXTRA_SUBJECT, sSubj);
+    i.putExtra(Intent.EXTRA_TEXT, sMain);
+    i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+    startActivity(i);
+    try{
+        startActivity(Intent.createChooser(i, "Send mail..."));
+    }
+    catch (android.content.ActivityNotFoundException ex){
+    }
+}
 
-
-
-        EditText mMain = (EditText) getView().findViewById(R.id.editMain);
-        EditText mSubj = (EditText) getView().findViewById(R.id.editSubj);
-
-
-        String sSubj = mSubj.getText().toString();
-        String sMain = mMain.getText().toString();
-
-        Intent i = new Intent(Intent.ACTION_SEND);
-        i.setType("message/rfc822");
-        i.putExtra(Intent.EXTRA_EMAIL, new String[]{"lcsatuiuc@gmail.com"});
-        i.putExtra(Intent.EXTRA_SUBJECT, sSubj);
-        i.putExtra(Intent.EXTRA_TEXT, sMain);
-        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(i);
-        try{
-            startActivity(Intent.createChooser(i, "Send mail..."));
-
-        }
-        catch (android.content.ActivityNotFoundException ex){
-
-        }
-
-        }
-
-
-
-
-
+/**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+public interface OnButtonFragmentInteractionListener {
+        // TODO: Update argument type and name
+    public void onButtonFragmentInteraction(View v);
+    }
 
 }
